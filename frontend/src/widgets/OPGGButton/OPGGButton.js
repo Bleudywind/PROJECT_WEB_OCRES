b2b038cfbf95cf8decff5eb39d6d714284a77eed
Ezvee <lucas.dessouki@gmail.com>
2020-11-28 06:50:21 +0100

    button 1.1

--- frontend/src/widgets/OPGGButton/OPGGButton.js
+++ frontend/src/widgets/OPGGButton/OPGGButton.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import opggLogo from './opgg-logo.png';
+import opggLogo from './opgg-logo.svg';
 import './OPGGButton.css'
 
-class OPGGButton extends React.Component{
-    constructor(props){
+class OPGGButton extends React.Component {
+    constructor(props) {
         super(props);
         this.openLink = () => { window.open(this.props.link); }
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <img src={ opggLogo } alt="OP.GG Logo" onClick={ this.openLink }/>
+                <button onClick={this.openLink}>
+                    <img src={opggLogo} alt="OP.GG Logo" />
+                </button>
             </div>
-          );
+
+        );
     }
 }
 
-OPGGButton.defaultProps={ link: '//euw.op.gg' };
+OPGGButton.defaultProps = { link: '//euw.op.gg' };
 
 export default OPGGButton;
+
