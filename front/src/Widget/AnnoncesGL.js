9072947818199dad964109c5e0cbff12e43bc2d1
Bleudywind <47596143+Bleudywind@users.noreply.github.com>
2020-11-30 05:05:58 +0100

    Name Folder

---
+++ front/src/Widget/AnnoncesGL.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './AnnoncesGL.css';
+
+class AnnonceGL extends React.Component {
+
+    constructor (props){
+        super(props);
+    }
+
+    render()
+}
