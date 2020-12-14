33c6e952e740bad3dce2060333c9a009cd8e16c0
Ezvee <lucas.dessouki@gmail.com>
2020-12-14 16:59:42 +0100

    queries wtf c'est insane mangodb en fait

--- backend/routes/matches.js
+++ backend/routes/matches.js
@@ -7,6 +7,18 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/incomingMatches').get((req, res) => {
+  Match.find({ "date": { "$gte": new Date("2020-11-10") } }).sort({ date: 1 }).limit(1)
+    .then(matches => res.json(matches))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/nextMatch').get((req, res) => {
+  Match.find({ "date": { "$gte": new Date("2020-11-10") } }, { "_id": 0, "opponents": 0, "__v": 0 }).sort({ date: 1 }).limit(1)
+    .then(matches => res.json(matches))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 router.route('/add').post((req, res) => {
   const date = Date.parse(req.body.date);
   const opponents = String(req.body.opponents)
@@ -17,8 +29,8 @@ router.route('/add').post((req, res) => {
   });
 
   newMatch.save()
-  .then(() => res.json('Match added!'))
-  .catch(err => res.status(400).json('Error: ' + err));
+    .then(() => res.json('Match added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
