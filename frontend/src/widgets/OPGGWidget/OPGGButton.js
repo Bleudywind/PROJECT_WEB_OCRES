13eed944fc02bcfd4ab736de081e28533d7833d3
Bleudywind <47596143+Bleudywind@users.noreply.github.com>
2020-12-15 14:54:11 +0100

    connection OPGG

--- frontend/src/widgets/OPGGWidget/OPGGButton.js
+++ frontend/src/widgets/OPGGWidget/OPGGButton.js
@@ -1,17 +1,80 @@
 import React from 'react';
 import opggLogo from './opgg-logo.svg';
 import './OPGGButton.css'
+import axios from 'axios';
+
+class Link extends React.Component {
+
+    openLink ()  {
+        var link;
+        var teamName;
+        console.log("ccc");
+        for (var i = 0; i < 10; i++) {
+            if (this.props.teams[i].teamName === this.props.matches.opponent) {
+                teamName = i;
+            }
+        }
+        link = "https://euw.op.gg/multi_old/query="+ this.props.teams[teamName].players[0].summonerName1 +"%2C"+ this.props.teams[teamName].players[0].summonerName2 +"%2C"+ this.props.teams[teamName].players[0].summonerName3 +"%2C"+ this.props.teams[teamName].players[0].summonerName4 +"%2C"+ this.props.teams[teamName].players[0].summonerName5;
+
+        console.log(link);
+        window.open(link);
+    }
+
+
+    render() {
+        return (
+            <button className="OPGGbutton" onClick={() => this.openLink()}>
+                <img className="OPGGimg" src={opggLogo} alt="OP.GG Logo" />
+            </button>
+        );
+    }
+}
+
 
 class OPGGButton extends React.Component {
-  
-    openLink = () => { window.open(this.props.link); }
+
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            matches: [],
+            teams: [],
+        };
+    }
+
+    componentDidMount() {
+
+        
+        axios.get('http://localhost:5000/matches/nextMatch')
+            .then(response => {
+                this.setState({ matches: response.data });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        axios.get('http://localhost:5000/teams/')
+            .then(responses => {
+                this.setState({ teams: responses.data });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+
+
+    matchList() {
+        return this.state.matches.map(currentmatches => {
+            return <Link matches={currentmatches} teams={this.state.teams} />
+        })
+    }
+
 
     render() {
         return (
             <div className='widgetContainer1'>
-                <button className="OPGGbutton" onClick={this.openLink}>
-                    <img className="OPGGimg" src={opggLogo} alt="OP.GG Logo" />
-                </button>
+                {this.matchList()}
             </div>
         );
     }
