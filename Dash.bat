bb734172cbb2d10315e100a46a0d3bd5596fef01
Bleudywind <47596143+Bleudywind@users.noreply.github.com>
2020-12-15 15:03:59 +0100

    .bat projet final

--- Dash.bat
+++ Dash.bat
@@ -1,6 +1,5 @@
 cd %~dp0\backend
 start /min nodemon server
-cd %~dp0\frontend\src\discordBot
-start node fetchGL.js
 cd %~dp0\frontend
+start /min node .\src\discordBot\fetchGL.js
 start /min npm start %~dp0\frontend
